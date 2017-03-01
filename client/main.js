e1ebb2ed8964f0a66427ae8c3ba3dcc3ceb047c4
David <david.huet.j.e@gmail.com>
2017-03-01 11:27:16 +0100

    route pour les villes ok

--- client/main.js
+++ client/main.js
@@ -6,15 +6,33 @@ Template.reset.events({
 	}
 });
 
-/*Template.city.helpers({
+Template.city.helpers({
+    
+
     places : function ()
     {
-        return Activities.find({"nature" : "place"});
+        console.log("city1="+this.activities);
+       // var city = Cities.findOne({"_id" : this._id});
+        //console.log("city2="+city);
+        var act = [];
+        //act = this.activities;
+        for (var i = 0; i < this.activities.length; i++ )
+        {
+            if (this.activities[i].nature == "place")
+                act.push(this.activities[i]);
+        }
+        return act;
     },
 
     events : function ()
     {
-        return Activities.find({"nature" : "event"});
+        var evt = [];
+        for (i = 0; i < this.activities.length; i++ )
+                {
+                    if (this.activities[i].nature == "event")
+                        evt.push(this.activities[i]);
+                }
+        return evt;
     },
 
-});*/
+});
