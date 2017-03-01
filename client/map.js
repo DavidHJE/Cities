47d87e05d3923ba024e7473d2f600a2287f9da39
limchengbin <callmebin95@gmail.com>
2017-03-01 12:03:27 +0100

    Update map.js

--- client/map.js
+++ client/map.js
@@ -3,13 +3,3 @@
     GoogleMaps.load();
   });
 
-  Template.city.helpers({
-    mapOptions: function() {
-      if (GoogleMaps.loaded()) {
-        return {
-          center: new google.maps.LatLng(-37.8136, 144.9631),
-          zoom: 8
-        };
-      }
-    }
-  });
