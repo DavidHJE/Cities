6df8cd9511dc953b1c610c2ded09c1753447861e
limchengbin <callmebin95@gmail.com>
2017-03-01 11:58:32 +0100

    Update map.js

---
+++ client/map.js
@@ -0,0 +1,15 @@
+
+  Meteor.startup(function() {
+    GoogleMaps.load();
+  });
+
+  Template.city.helpers({
+    mapOptions: function() {
+      if (GoogleMaps.loaded()) {
+        return {
+          center: new google.maps.LatLng(-37.8136, 144.9631),
+          zoom: 8
+        };
+      }
+    }
+  });
