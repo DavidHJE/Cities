beea21ab7c4711bcad2b8d87badaadec993ffb4c
antoine <antoine@meresse.info>
2017-03-01 11:42:28 +0100

    reorganisation

---
+++ client/city.js
@@ -0,0 +1,30 @@
+Template.city.helpers({
+
+
+    places : function ()
+    {
+        console.log("city1="+this.activities);
+        // var city = Cities.findOne({"_id" : this._id});
+        //console.log("city2="+city);
+        var act = [];
+        //act = this.activities;
+        for (var i = 0; i < this.activities.length; i++ )
+        {
+            if (this.activities[i].nature == "place")
+                act.push(this.activities[i]);
+        }
+        return act;
+    },
+
+    events : function ()
+    {
+        var evt = [];
+        for (i = 0; i < this.activities.length; i++ )
+        {
+            if (this.activities[i].nature == "event")
+                evt.push(this.activities[i]);
+        }
+        return evt;
+    },
+
+});
