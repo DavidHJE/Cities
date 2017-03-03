97db9fa2a770bbe37d85ae0563bc0c65eb7af0bc
chengbin.lim <chengbin.lim@103f24.iut-lens.univ-artois.fr>
2017-03-03 09:21:30 +0100

    asd

--- client/charts.js
+++ client/charts.js
@@ -1,40 +1,35 @@
-Template.charts.onRendered(function(){
-    var ctx = document.getElementById("myChart");
-    var myChart = new Chart(ctx,{
-        type:'bar',
-        data:{
-            labels:["Red","Blue","Yellow","Green","Purple","Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
+Template.charts.onRendered(()=>{
+    let ctx = $("#myChart").get(0).getContext("2d");
+    
+    let data = (
+        labels:["January","February","March","April","May","June","July","August","September","October","November","December"],
+        datasets: [] 
+    );
+    
+    data.datasets.push({
+       label: "temp max",
+        fillColor: "rgba(220,220,220,0.2)",
+        strokeColor: "rgba(220,220,220,1)",
+        pointColor: "rgba(220,220,220,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(220,220,220,1)",
+        data: [25,27,25,20,18,15,15,16,20,22,22,24]
     });
- 
+    
+    data.datasets.push({
+        label: "temp min",
+        fillColor: "rgba(151,187,205,0.2)",
+        strokeColor: "rgba(151,187,205,1)",
+        pointColor: "rgba(151,187,205,1)",
+        pointStrokeColor: "#fff",
+        pointHighlightFill: "#fff",
+        pointHighlightStroke: "rgba(151,187,205,1)",
+        data: []
+    });
+    for(let i=0;i<12;i++){
+        data.datasets[1].data.push(3+Math.random()*10);
+    }
+    
+    new Chart(ctx).Line(data, Chart.defaults.global);
 });
