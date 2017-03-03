33c911950905b65b9a7306076ce568f4e3728803
chengbin.lim <chengbin.lim@103f24.iut-lens.univ-artois.fr>
2017-03-03 13:12:22 +0100

    charts

--- client/charts.js
+++ client/charts.js
@@ -1,35 +1,52 @@
-Template.charts.onRendered(()=>{
+Template.charts.onRendered(function () {
+    Chart.defaults.global.scaleBeginAtZero = true;
+    // get graphics context of canvas elements
+    // and call global methods on the server
+    // for data aggregation
+    let ct = Template.currentData().name.replace(/ /g, "-");
+    
     let ctx = $("#myChart").get(0).getContext("2d");
-    
-    let data = (
-        labels:["January","February","March","April","May","June","July","August","September","October","November","December"],
-        datasets: [] 
-    );
-    
-    data.datasets.push({
-       label: "temp max",
-        fillColor: "rgba(220,220,220,0.2)",
-        strokeColor: "rgba(220,220,220,1)",
-        pointColor: "rgba(220,220,220,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(220,220,220,1)",
-        data: [25,27,25,20,18,15,15,16,20,22,22,24]
+    Meteor.call("getWeatherData", ct, (err, result) => {
+        let data = {
+        labels: [],
+        datasets: []
+      };
+      data.datasets.push({
+          label: "temp",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: []
+      });
+      data.datasets.push({
+          label: "temp min",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: []
+      });
+      data.datasets.push({
+          label: "temp max",
+          fillColor: "rgba(255,187,205,0.2)",
+          strokeColor: "rgba(255,187,205,1)",
+          pointColor: "rgba(255,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(255,187,205,1)",
+          data: []
+      });
+      for (let d of result.list) {
+        data.labels.push(d.dt_txt);
+        data.datasets[0].data.push(d.main.temp);
+        data.datasets[1].data.push(d.main.temp_min);
+        data.datasets[2].data.push(d.main.temp_max);
+      }
+      new Chart(ctx).Line(data, Chart.defaults.global);
     });
-    
-    data.datasets.push({
-        label: "temp min",
-        fillColor: "rgba(151,187,205,0.2)",
-        strokeColor: "rgba(151,187,205,1)",
-        pointColor: "rgba(151,187,205,1)",
-        pointStrokeColor: "#fff",
-        pointHighlightFill: "#fff",
-        pointHighlightStroke: "rgba(151,187,205,1)",
-        data: []
-    });
-    for(let i=0;i<12;i++){
-        data.datasets[1].data.push(3+Math.random()*10);
-    }
-    
-    new Chart(ctx).Line(data, Chart.defaults.global);
 });
